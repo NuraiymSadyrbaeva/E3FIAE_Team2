1772d69d4e44b8a496ecaf9d68c776f11f536ee5
proasmaasoukoun-cloud <pro.asmaasoukoun@gmail.com>
2025-10-31 23:24:43 +0000

    Update auf die readme seite

--- project_chatbot/scripts/main.py
+++ project_chatbot/scripts/main.py
@@ -6,11 +6,9 @@ app = Flask(
     template_folder="templates",
     static_folder="static",
 )
-
 @app.route("/")
 def home():
     return render_template("index.html", title="YourChatbot")
 
 if __name__ == "__main__":
-    # 5050 ist sicher auf deinem Mac (5000 ist oft belegt)
     app.run(debug=True, port=5050)
